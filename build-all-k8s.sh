7f10132408f1f5360ec224f0e7386e5f800656df
Chiran Fernando <pcnfernando@gmail.com>
2022-12-12 16:42:52 +0400

    Update and rename build-all-minikube.sh to build-all-k8s.sh

--- build-all-k8s.sh
+++ build-all-k8s.sh
@@ -14,7 +14,6 @@
 # limitations under the License.
 #
 
-eval $(minikube docker-env)
 ( cd client_stubs ; bal pack ; bal push --repository local)
 ( cd money ; bal pack ; bal push --repository local)
 ( cd cartservice ; bal build --cloud=k8s)
